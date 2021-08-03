78450e07b15e04176ee8d8ce708a6775ac153559
Ui Sam <uisam.santos@gmail.com>
2021-08-02 22:40:24 -0300

    fix: arrumando componente parceiros

--- static/i18n/en.js
+++ static/i18n/en.js
@@ -1,22 +1,25 @@
 export default {
-    TOOLBAR: {
-        title: "Young Exporter Institute",
-        title_resume: 'I. Young Exporter',
-        menu: {
-            about_us: 'ABOUT US',
-            our_services: 'OUR SERVICES',
-            blog: 'BLOG',
-            events: 'EVENTS',
-            contact_us: 'CONTACT US',
-        }
+  TOOLBAR: {
+    title: 'Young Exporter Institute',
+    title_resume: 'I. Young Exporter',
+    menu: {
+      about_us: 'ABOUT US',
+      our_services: 'OUR SERVICES',
+      blog: 'BLOG',
+      events: 'EVENTS',
+      contact_us: 'CONTACT US',
     },
-    HOME: {
-        card: {
-            title_first: "Access",
-            title_second: "your",
-            title_third: "region!",
-            text_first: "JOIN",
-            text_second: " to the Young Exporter Program Center in your region."
-        }
-    }
+  },
+  HOME: {
+    card: {
+      title_first: 'Access',
+      title_second: 'your',
+      title_third: 'region!',
+      text_first: 'JOIN',
+      text_second: ' to the Young Exporter Program Center in your region.',
+    },
+  },
+  PARTNERS: {
+    title: 'Partners',
+  },
 }
--- static/i18n/pt.js
+++ static/i18n/pt.js
@@ -1,22 +1,25 @@
 export default {
-    TOOLBAR: {
-        title: 'Instituto Jovem Exportador',
-        title_resume: 'I. Jovem Exportador',
-        menu: {
-            about_us: 'QUEM SOMOS',
-            our_services: 'NOSSOS SERVIÇOS',
-            blog: 'BLOG',
-            events: 'EVENTOS',
-            contact_us: 'FALE CONOSCO',
-        }
+  TOOLBAR: {
+    title: 'Instituto Jovem Exportador',
+    title_resume: 'I. Jovem Exportador',
+    menu: {
+      about_us: 'QUEM SOMOS',
+      our_services: 'NOSSOS SERVIÇOS',
+      blog: 'BLOG',
+      events: 'EVENTOS',
+      contact_us: 'FALE CONOSCO',
     },
-    HOME: {
-        card: {
-            title_first: "Acesse",
-            title_second: "sua",
-            title_third: "região!",
-            text_first: "ASSOCIE-SE",
-            text_second: " ao Núcleo do Programa Jovem Exportador de sua região."
-        }
-    }
+  },
+  HOME: {
+    card: {
+      title_first: 'Acesse',
+      title_second: 'sua',
+      title_third: 'região!',
+      text_first: 'ASSOCIE-SE',
+      text_second: ' ao Núcleo do Programa Jovem Exportador de sua região.',
+    },
+  },
+  PARTNERS: {
+    title: 'Parceiros',
+  },
 }
