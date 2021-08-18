b5eb98739a609e03af3187f9d90c52bfd8e4e64c
Milad Roghanian <miladr100@gmail.com>
2021-08-17 23:10:55 -0400

    feat: sobre-nos

--- static/i18n/pt.js
+++ static/i18n/pt.js
@@ -40,4 +40,9 @@ export default {
   PARTNERS: {
     title: 'Parceiros',
   },
+  ABOUT: {
+    title_first: 'SOBRE O INSTITUTO',
+    title_second: 'JOVEM EXPORTADOR',
+    text: 'O Instituto Jovem Exportador é uma organização não governamental, sem fins lucrativos, que, através de um programa de inclusão e fomento do desenvolvimento sócio-econômico, atua na sensibilização e capacitação de empresários brasileiros para o comércio  exterior. As ações tem foco nas micro, pequenas e médias empresas e visam promover as exportações e valorizar os produtos e serviços brasileiros para acesso ao mercado intencional.',
+  }
 }
