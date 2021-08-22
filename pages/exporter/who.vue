532d084bf8dc8216aed0bd09e9ad1c8e3821c9d1
Milad Roghanian <miladr100@gmail.com>
2021-08-21 22:40:44 -0400

    feat: sobre nos

---
+++ pages/exporter/who.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-row>
+    <v-row v-if="loadingScreen" justify="center" align="center">
+			<v-progress-circular indeterminate :size="80" color="teal"></v-progress-circular>
+		</v-row>
+    <v-row v-else justify="center" class="mx-md-12 mt-4">
+        <v-col cols="10">
+          <v-card class="overflow-hidden" flat color="green lighten-4">
+            <v-card-title class="d-flex justify-center">{{$t('WHO.about_title')}}</v-card-title>
+            <v-card-text class="text-subtitle-1 text-justify">{{$t('WHO.about_description_first')}}</v-card-text>
+            <v-card-text class="text-subtitle-1 text-justify mt-n7">{{$t('WHO.about_description_second')}}</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="10" class="mt-lg-n12">
+          <v-row>
+            <v-col cols="12" lg="6" ms="6" sm="12">
+              <v-card class="overflow-hidden" flat color="light-green lighten-4">
+                <v-card-title class="d-flex justify-center">{{$t('WHO.vision_title')}}</v-card-title>
+                <v-card-text class="text-subtitle-1 text-justify">{{$t('WHO.vision_description')}}</v-card-text>
+              </v-card>
+            </v-col>
+            <v-col cols="12" lg="6" ms="6" sm="12">
+              <v-card class="overflow-hidden" flat color="light-green lighten-4">
+                <v-card-title class="d-flex justify-center">{{$t('WHO.mission_title')}}</v-card-title>
+                <v-card-text class="text-subtitle-1 text-justify">{{$t('WHO.mission_description')}}</v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+    </v-row>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  computed: {
+    loadingScreen() {
+      return this.$store.getters.getStateLoadingScreen
+    },
+  },
+  mounted() {
+    this.$store.dispatch('setLoadingScreen', 1000)
+  },
+}
+</script>
--- static/i18n/en.js
+++ static/i18n/en.js
@@ -10,8 +10,9 @@ export default {
       exporter: {
         title: 'YOUNG EXPORTER',
         submenu: {
-          about_us: 'ABOUT US', 
+          about_us: 'WHO WE ARE', 
           team: 'OUR TEAM',
+          who: 'ABOUT US',
         }
       },
       our_services: {
@@ -44,5 +45,14 @@ export default {
     title_first: 'SOBRE O INSTITUTO',
     title_second: 'JOVEM EXPORTADOR',
     text: 'O Instituto Jovem Exportador é uma organização não governamental, sem fins lucrativos, que, através de um programa de inclusão e fomento do desenvolvimento sócio-econômico, atua na sensibilização e capacitação de empresários brasileiros para o comércio  exterior. As ações tem foco nas micro, pequenas e médias empresas e visam promover as exportações e valorizar os produtos e serviços brasileiros para acesso ao mercado intencional.',
+  },
+  WHO: {
+    about_title: "ABOUT THE YOUNG EXPORTER",
+    about_description_first: "The Instituto Jovem Exportador is a non-governmental, non-profit organization that, through a program of inclusion and promotion of socioeconomic development, works to raise awareness and train Brazilian businessmen for foreign trade.",
+    about_description_second: "The actions are focused on micro and small companies and aim to promote exports and value Brazilian products and services for access to the intentional market.",
+    vision_title: "OUR VISION",
+    vision_description: "To be recognized nationally and internationally by entrepreneurs, entrepreneurs and investors as the main platform for inclusion, connection and access of micro and small businesses to new markets.",
+    mission_title: "OUR MISSION",
+    mission_description: "Promote the dissemination of a culture of business internationalization for young entrepreneurs and entrepreneurs, developing skills and connecting them with new markets.",
   }
 }
