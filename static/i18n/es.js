8839e8719d88a58f7644ac57f5dea510c6ace6a2
Ui Sam <uisam.santos@gmail.com>
2021-08-22 01:36:41 -0300

    fix: tela de sobre nós ajustada, tradução feita

--- static/i18n/es.js
+++ static/i18n/es.js
@@ -10,7 +10,7 @@ export default {
       exporter: {
         title: 'JÓVENES EXPORTADORES',
         submenu: {
-          about_us: 'QUIENES SOMOS', 
+          about_us: 'QUIENES SOMOS',
           team: 'NUESTRO EQUIPO',
         }
       },
@@ -42,6 +42,20 @@ export default {
     title: 'Socios',
   },
   ABOUT: {
+    card_member: {
+      president: 'Presidente',
+      institutional_relations: 'Director de Relaciones Institucionales y Gubernamentales',
+      content_director: 'Director de Contenido',
+      technical_director: 'Director Técnico',
+      ceo: 'Director Ejecutivo',
+      administrative_financial_director: 'Director Administrativo y Financiero',
+      legal_board: 'Junta Legal',
+      cplp_Board: 'Junta CPLP',
+      strategic_projects_board: 'Director de Proyectos Estratégicos y Asuntos Internacionales',
+      executive_secretary: 'Secretaria Executiva'
+
+
+    },
     title_first: 'SOBRE O INSTITUTO',
     title_second: 'JOVEM EXPORTADOR',
     text: 'O Instituto Jovem Exportador é uma organização não governamental, sem fins lucrativos, que, através de um programa de inclusão e fomento do desenvolvimento sócio-econômico, atua na sensibilização e capacitação de empresários brasileiros para o comércio  exterior. As ações tem foco nas micro, pequenas e médias empresas e visam promover as exportações e valorizar os produtos e serviços brasileiros para acesso ao mercado intencional.',
--- static/i18n/zh.js
+++ static/i18n/zh.js
@@ -10,7 +10,7 @@ export default {
       exporter: {
         title: '年轻的出口商',
         submenu: {
-          about_us: '我们是谁', 
+          about_us: '我们是谁',
           team: '我们的队伍',
         }
       },
@@ -41,6 +41,20 @@ export default {
     title: '夥伴',
   },
   ABOUT: {
+    card_member: {
+      president: '總統',
+      institutional_relations: '機構和政府關係官員',
+      content_director: '內容總監',
+      technical_director: '技術總監',
+      ceo: '首席執行官',
+      administrative_financial_director: '行政和財務總監',
+      legal_board: '法律委員會',
+      cplp_Board: 'CPLP 委員會',
+      strategic_projects_board: '戰略項目和國際事務局',
+      executive_secretary: '行政秘書'
+
+
+    },
     title_first: 'SOBRE O INSTITUTO',
     title_second: 'JOVEM EXPORTADOR',
     text: 'O Instituto Jovem Exportador é uma organização não governamental, sem fins lucrativos, que, através de um programa de inclusão e fomento do desenvolvimento sócio-econômico, atua na sensibilização e capacitação de empresários brasileiros para o comércio  exterior. As ações tem foco nas micro, pequenas e médias empresas e visam promover as exportações e valorizar os produtos e serviços brasileiros para acesso ao mercado intencional.',
